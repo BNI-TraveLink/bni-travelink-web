af557cefc84a138c21faf190e1a0a739c93637d4
roningrum <rizkioktavianingrum@gmail.com>
2024-01-17 16:20:17 +0700

    buat komponen & routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PaymentComponent } from './payment/payment.component';
+import { PemesananComponent } from './pemesanan/pemesanan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PaymentComponent,
+    PemesananComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pemesanan/pemesanan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pemesanan',
+  templateUrl: './pemesanan.component.html',
+  styleUrl: './pemesanan.component.scss'
+})
+export class PemesananComponent {
+
+}
