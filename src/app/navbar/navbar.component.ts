10251d805b1d0dd60f166215afe2e6222fdead1a
roningrum <rizkioktavianingrum@gmail.com>
2024-02-01 11:14:13 +0700

    navbar done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { LoginService } from '../service/login.service';
+import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +10,19 @@ import { LoginService } from '../service/login.service';
 })
 export class NavbarComponent {
 
-  constructor(private service:LoginService){}
+  constructor(private service:LoginService, private route:Router, @Inject(DOCUMENT) private document: Document){}
+
+  home(){
+    this.route.navigate([''])
+  }
+
+  aboutUs(){
+    this.document.location.href= 'https://www.bni.co.id/id-id/'
+  }
+
+  myOrder(){
+    this.route.navigate(['myorder'])
+  }
 
   logout(){
     this.service.logout()
