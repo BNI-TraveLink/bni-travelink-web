a484c1b902af7e08d26d284e45ce2b8b5c511ef0
roningrum <rizkioktavianingrum@gmail.com>
2024-02-02 19:28:55 +0700

    tab

--- src/app/tab/tab.component.ts
+++ src/app/tab/tab.component.ts
@@ -4,10 +4,10 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../service/home.service';
 import { Station } from '../models/stations';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, flatMap, switchMap } from 'rxjs';
 import { Router } from '@angular/router';
-import { FormKRLDataService } from '../service/form.krl.service';
 import { Stepper } from '../service/stepper.service';
+
 
 @Component({
   selector: 'app-tab',
@@ -37,31 +37,35 @@ export class TabComponent implements OnInit {
 
   constructor(private homeService: HomeService, private router: Router, private stepper:Stepper) { }
   ngOnInit() {
+    
     this.getStationByTab(this.activeTab)
+    
     this.departureControl.valueChanges.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((value) => this.homeService.searchStation(value))
+      switchMap((value) => this.homeService.searchStation(value)),
     ).subscribe((result) => {
-
       this.searchedStation = result.length > 0 ? result : [{ station_name: 'No result' }];
       this.isSearching = true;
-    })
-
-    this.destinationControl.valueChanges.pipe(debounceTime(300),
+    });
+    
+    this.destinationControl.valueChanges.pipe(
+      debounceTime(300),
       distinctUntilChanged(),
-      switchMap((value) => this.homeService.searchDestination(value))
+      switchMap((value) => this.homeService.searchDestination(value)),
     ).subscribe((result) => {
       this.searchedDestination = result.length > 0 ? result : [{ station_name: 'No result' }];
       this.isSearchingDestination = true;
-    })
+    });
 
   }
-  selectedStation(stations: Station) {
-    this.departureControl.setValue(stations.station_name)
-    this.departure = this.departureControl.value
-    console.log('Tujuan', this.departure)
-    this.isSearching = false
+  selectedStation(stations: Station) { 
+    const selectedStation = stations.station_name
+    if(selectedStation !== this.departureControl.value){
+      this.departureControl.setValue(selectedStation)
+      this.departure = this.departureControl.value
+      this.isSearching = false
+    }
   }
 
   selectedDestination(stations: Station) {
@@ -70,6 +74,21 @@ export class TabComponent implements OnInit {
     this.isSearchingDestination = false
   }
 
+  handleDepartureFocus(): void {
+    if (this.searchedStation.length > 0) {
+      this.isSearching = true;
+    } else {
+      this.isSearching = false;
+    }
+  }
+  
+  handleDestinationFocus(): void {
+    if (this.searchedDestination.length > 0) {
+      this.isSearchingDestination = true;
+    } else {
+      this.isSearchingDestination = false;
+    }
+  }
 
   handleDepartureKeydown(event: KeyboardEvent): void {
     if (event.key === 'Enter') {
@@ -88,15 +107,14 @@ export class TabComponent implements OnInit {
 
   handleDestinationKeydown(event: KeyboardEvent): void {
     if (event.key === 'Enter') {
-      // Pilih stasiun pertama otomatis jika hasil pencarian tidak kosong
       if (this.searchedDestination.length > 0) {
         this.destinationControl.setValue(this.searchedDestination[0].station_name);
-        // this.destinationControl.setValue(this.searchedDestination[0].station_name)
         this.isSearchingDestination = false;
         event.preventDefault(); // Sembunyikan hasil pencarian setelah Enter
       }
       else {
         this.destinationControl.setValue('');
+        event.preventDefault();
       }
     }
   }
@@ -105,6 +123,14 @@ export class TabComponent implements OnInit {
   openCity(cityName: string): void {
     this.activeTab = cityName;
     this.getStationByTab(cityName)
+
+    this.departureControl.setValue('');
+    this.destinationControl.setValue('');
+    this.searchedDestination = [];
+    this.searchedStation = [];
+
+    this.isSearching = false;
+    this.isSearchingDestination = false;
   }
 
   getStationByTab(cityName:string){
@@ -120,6 +146,8 @@ export class TabComponent implements OnInit {
     const departureValue = this.departureControl.value;
     this.departureControl.setValue(this.destinationControl.value);
     this.destinationControl.setValue(departureValue);
+    this.searchedDestination =[]
+    this.searchedStation =[]
   }
 
   submitForm(cityName: string): void {
@@ -133,7 +161,11 @@ export class TabComponent implements OnInit {
       this.stepper.setBooleanValue(true)
       this.stepper.setisOrderValue(false)
       this.stepper.setCompleteValue(false)
-      
+
+      if (this.departure === this.destination) {
+        alert("Tujuan dan Asal tidak boleh sama");
+        return;
+      }
       this.navigateToPay()
       }
       else{
